9cb6d1be28011f126fe0581be76ec38170af3518
yaz10 <webdevyaz10@gmail.com>
2026-01-05 19:09:06 +0300

    add backend practice file

---
+++ data/backend-practice.js
@@ -0,0 +1,14 @@
+//# XMLHttpRequest is a built-in object that allows you to make HTTP requests to a server.
+//? request is the message that is sent to the server.
+//? response is the message that is received from the server.
+const xhr = new XMLHttpRequest(); //$ this is an asyncrounous code
+
+xhr.addEventListener('load', () => { //$ this is a callback function (a function that is called when the event happens)
+    console.log(xhr.response);
+});
+
+xhr.open('GET','https://supersimplebackend.dev') //? this sets up the message (it will be a Get message here)
+xhr.send() //? this sends the message
+// xhr.response //? U can also get the response and use it 
+//! but wait!! the response is undefined!! because the response takes time to come back from the server
+//! so we need to use a callback function 
